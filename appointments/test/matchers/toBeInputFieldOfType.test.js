27f91eca542be1e7a389ea31f77edef7a27e76de
abraham <abrahamsc1980@gmail.com>
2023-05-02 13:27:53 +0000

    Completed toBeInputFieldOfType test

--- appointments/test/matchers/toBeInputFieldOfType.test.js
+++ appointments/test/matchers/toBeInputFieldOfType.test.js
@@ -2,6 +2,8 @@ import { toBeInputFieldOfType } from "./toBeInputFieldOfType";
 
 describe('toBeInputFieldOfType', () => {
 
+    const stripTerminalColor = (text) => text.replace(/\x1B\[\d+m/g, "");
+
     const createElement = (elementStr) => {
         const parent = document.createElement("div");
         parent.innerHTML = elementStr;
@@ -34,12 +36,32 @@ describe('toBeInputFieldOfType', () => {
         const result = toBeInputFieldOfType(element, 'text');
 
         expect(result.pass).toBe(false);
-
     });
 
     it('returns a message that contains the source line if the negated match', () => {
+        const element = createElement('<input type="text" />');
+        const result = toBeInputFieldOfType(element, 'text');
+        
+        expect(stripTerminalColor(result.message())).toMatch('expect(element).not.toBeInputFieldOfType("text")');
+    });
+
+    it('returns a specific message when the element passed is null', () => {
+        const result = toBeInputFieldOfType(null, 'text');
+
+        expect(stripTerminalColor(result.message())).toMatch(`Actual: element was not found`);
+    });
+
+    it('returns a message when the element has the wrong tag', () => {
+        const element = createElement('<p />');
+        const result = toBeInputFieldOfType(element, 'text');
+
+        expect(stripTerminalColor(result.message())).toMatch(`Actual: <p>`);
+    });
+
+    it('returns a message when the input element has the wrong type', () => {
         const element = createElement('<input type="date" />');
         const result = toBeInputFieldOfType(element, 'text');
-        expect(result.message()).toMatch('expect(element).not.toBeInputFieldTypeOf("text")');
+
+        expect(stripTerminalColor(result.message())).toMatch(`Actual: <input type="date" />`);
     });
 });
