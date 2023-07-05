a9e83fe5e687967fbc8fd8af208185cfe747ef2d
abraham <abrahamsc1980@gmail.com>
2023-07-05 14:29:06 -0400

    Added App component to the root

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { sampleAppointments, sampleAvailableTimeSlots } from './sampleData';
-import { AppointmentForm } from './AppointmentForm';
+import { App } from './App';
 
 ReactDOM
     .createRoot(document.getElementById("root"))
-    .render(
-        <AppointmentForm
-        original={{}}
-        availableTimeSlots={sampleAvailableTimeSlots}
-        appointments={sampleAppointments}
-        onSubmit={() => {}}
-      />
-    );
+    .render(<App />);
