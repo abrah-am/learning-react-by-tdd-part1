41cf7322059eeaa5a510eea03fa6ed6a21b148ab
abraham <abrahamsc1980@gmail.com>
2023-08-23 12:09:41 -0400

    CH12-Making asynchronous requests with sagas

--- test/sagas/customer.test.js
+++ test/sagas/customer.test.js
@@ -1,12 +1,21 @@
 import { expectRedux, storeSpy } from "expect-redux";
 import { configureStore } from "../../src/store";
+import { fetchResponseError, fetchResponseOk } from '../builders/fetch';
 
 describe('addCustomer', () => {
+    
     let store;
-    beforeEach(() => {
-        store = configureStore([storeSpy])
-    });
 
+    const customer = { id: 123 };
+
+    beforeEach(() => {
+        jest
+          .spyOn(global, "fetch")
+          .mockReturnValue(fetchResponseOk(customer));
+        store = configureStore([storeSpy]);
+      });
+
+      
     const addCustomerRequest = (customer) => ({
         type: 'ADD_CUSTOMER_REQUEST',
         customer
@@ -18,6 +27,77 @@ describe('addCustomer', () => {
             .toDispatchAnAction()
             .matching({
                 type: 'ADD_CUSTOMER_SUBMITTING'
+            });
+    });
+
+    it('sends HTTP request to POST /customers', async () => {
+        const inputCustomer = { firstName: 'Ashley' };
+        store.dispatch(addCustomerRequest(inputCustomer));
+        expect(global.fetch).toBeCalledWith(
+            '/customers',
+            expect.objectContaining({
+                method: 'POST'
+            })
+        );
+    });
+
+    it('calls fetch with correct configuration', async () => {
+        const inputCustomer = { firstName: 'Ashley' };
+        store.dispatch(addCustomerRequest(inputCustomer));
+        expect(global.fetch).toBeCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+                credentials: 'same-origin',
+                headers: { 'Content-Type': 'application/json' }
+            })
+        );
+    });
+
+    it('calls fetch with customer as request body', async () => {
+        const inputCustomer = { firstName: 'Ashley' };
+        store.dispatch(addCustomerRequest(inputCustomer));
+        expect(global.fetch).toBeCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+                body: JSON.stringify(inputCustomer)
+            })
+        );
+    });
+
+    it('dispatches ADD_CUSTOMER_SUCCESSFUL on success', () => {
+        store.dispatch(addCustomerRequest());
+        return expectRedux(store)
+            .toDispatchAnAction()
+            .matching({
+                type: 'ADD_CUSTOMER_SUCCESSFUL',
+                customer
+            });
+    });
+
+    it('dispatches ADD_CUSTOMER_FAILED on non-specific error', () => {
+        global.fetch.mockReturnValue(fetchResponseError());
+        store.dispatch(addCustomerRequest());
+        return expectRedux(store)
+            .toDispatchAnAction()
+            .matching({
+                type: 'ADD_CUSTOMER_FAILED'
+            })
+    });
+
+    it('dispatches ADD_CUSTOMER_VALIDATON_FAILED if validation errors were returned', () => {
+        const errors = {
+            field: 'field',
+            description: 'error text'
+        };
+        global.fetch.mockReturnValue(
+            fetchResponseError(422, { errors })
+        );
+        store.dispatch(addCustomerRequest());
+        return expectRedux(store)
+            .toDispatchAnAction()
+            .matching({
+                type: 'ADD_CUSTOMER_VALIDATION_FAILED',
+                validationErrors: errors
             });
     });
 });
